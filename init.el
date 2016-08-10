bcb606e4ad32a75a2572937c9cbfe740045763be
David Toomey <dbtoomey@gmail.com>
2016-08-09 23:08:38 -0500

    update

--- init.el
+++ init.el
@@ -1,3 +1,23 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(package-selected-packages (quote (paredit cider clojure-mode avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 ;; add melpa
 (require 'package)
 (add-to-list 'package-archives
@@ -53,5 +73,5 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-;; add manaj dark theme
-(load-theme 'manoj-dark)
+;; load python3 interpretor by default
+(setq python-shell-interpreter "python3")
